658ddf68b67fcf7a09bdc0d5667f0f63c6b27486
sachinkumawat012 <sachinkumawat012@gmail.com>
2022-07-21 22:15:21 +0530

    Updates

---
+++ precache-manifest.504acca345f4df283b6c76c8ad51798a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b571f621a4d9bb972078",
+    "url": "/ReactLive/static/css/main.c1a3e1e3.chunk.css"
+  },
+  {
+    "revision": "b571f621a4d9bb972078",
+    "url": "/ReactLive/static/js/main.b571f621.chunk.js"
+  },
+  {
+    "revision": "fa6fed4e7390d6c1600c",
+    "url": "/ReactLive/static/css/1.6abd3a0a.chunk.css"
+  },
+  {
+    "revision": "fa6fed4e7390d6c1600c",
+    "url": "/ReactLive/static/js/1.fa6fed4e.chunk.js"
+  },
+  {
+    "revision": "9ec8c75b89f057e4b1cb",
+    "url": "/ReactLive/static/js/runtime~main.9ec8c75b.js"
+  },
+  {
+    "revision": "7fccd783bb28ecab0fd9e11b1eeb71d6",
+    "url": "/ReactLive/static/media/about.7fccd783.jpg"
+  },
+  {
+    "revision": "24fa0449f25de2e6edc32b54b474ca73",
+    "url": "/ReactLive/static/media/serv.24fa0449.jpg"
+  },
+  {
+    "revision": "73fe00210a3447e800fb04a729910930",
+    "url": "/ReactLive/static/media/c1.73fe0021.avif"
+  },
+  {
+    "revision": "85c11924111ef76000e03e9493a37086",
+    "url": "/ReactLive/static/media/c2.85c11924.avif"
+  },
+  {
+    "revision": "d46543ac57c001e1b56728d5c8098bbe",
+    "url": "/ReactLive/static/media/c3.d46543ac.avif"
+  },
+  {
+    "revision": "00f7b0c2953de1e055dfc562f7e5cda6",
+    "url": "/ReactLive/static/media/c4.00f7b0c2.avif"
+  },
+  {
+    "revision": "638858a70d070f14f66ce3c82b83221a",
+    "url": "/ReactLive/static/media/c5.638858a7.jpg"
+  },
+  {
+    "revision": "701822f6171cef9c43ef24e0e5f78ed2",
+    "url": "/ReactLive/static/media/c6.701822f6.jpg"
+  },
+  {
+    "revision": "a739b18327dd9d0efe3ea716ca51c739",
+    "url": "/ReactLive/static/media/cont.a739b183.jpg"
+  },
+  {
+    "revision": "ecd0e83525b53ba11ef7d330911bb57d",
+    "url": "/ReactLive/index.html"
+  }
+];
