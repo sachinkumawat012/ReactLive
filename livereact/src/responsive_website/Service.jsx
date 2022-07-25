d27b70a60ada84511e2f6a0af44bdfe02d8f1a58
Sachin Kumawat <90393935+sachinkumawat012@users.noreply.github.com>
2022-07-25 17:43:47 +0530

    Revert "Modified package.json file"

--- livereact/src/responsive_website/Service.jsx
+++ livereact/src/responsive_website/Service.jsx
@@ -18,11 +18,15 @@ const Service = () =>{
         </div>
         <div className="container-fluid mb-5 ">
             <div className="row">
+                <div className="col-10 mx-auto">
+                    <div className="row gy-5">
                         {Sdata.map((val, index) =>{
                               return(<Card key={index} imgsrc = {val.imsrc} title = {val.title} />)})
                         }
                     </div>
                 </div>
+            </div>
+        </div>
 
         </>
 );
